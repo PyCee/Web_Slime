f7947f28382f69f827ee5a9b89848173396b97cc
Kim <Kpampusch@yahoo.com>
2017-12-11 22:49:24 -0600

    added gap in bottom arena_map

--- js/arena_map.js
+++ js/arena_map.js
@@ -1,8 +1,8 @@
-var arena_width = 10.0;
+var arena_width = 12.0;
 var arena_height = arena_width * canvas_dimensions.aspect_ratio.multiplier;
 
 var arena = new Map(arena_width, function(){
-    slime.position = new Vector(4.75, 4.0);
+    slime.position = new Vector(5.75, 6.0);
 });
 arena.add_actor(slime);
 // Walls
@@ -11,8 +11,13 @@ arena.add_actor(new Actor(new Vector(0.0, 0.0),
 			  new Vector(arena_width, 1.0),
 			  new Sprite("black.png")));
 // Bottom wall
+// Bottom Left
 arena.add_actor(new Actor(new Vector(0.0, arena_height - 0.3),
-			  new Vector(arena_width, 0.3),
+			  new Vector(5.5, 0.3),
+			  new Sprite("black.png")));
+// Bottom Right
+arena.add_actor(new Actor(new Vector(6.5, arena_height - 0.3),
+			  new Vector(5.5, 0.3),
 			  new Sprite("black.png")));
 // Left wall
 arena.add_actor(new Actor(new Vector(0.0, 0.0),
