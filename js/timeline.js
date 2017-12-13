3c8e8e993435fca0d7375cffdb62a2d4ed5fc096
Kim <Kpampusch@yahoo.com>
2017-12-12 18:08:49 -0600

    added timeline reset

--- js/timeline.js
+++ js/timeline.js
@@ -8,6 +8,9 @@ class Timeline {
     start () {this.active = true;}
     stop () {this.active = false;}
     set (time) {this.elapsed_time = time;}
+    reset () {
+	this.set(0.0);
+    }
     update (delta_s) {
 	if(this.active){
 	    this.elapsed_time += delta_s;
