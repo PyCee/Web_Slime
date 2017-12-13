0df30340307d4c47c37160f9a95ff4e77ce91926
Kim <Kpampusch@yahoo.com>
2017-12-12 18:08:35 -0600

    added pseudocode for combat update

--- js/combat_scene.js
+++ js/combat_scene.js
@@ -3,23 +3,32 @@ var enemy_party = new Party();
 
 var slime_character = new Character("Slime", "green.png");
 player_party.add_member(slime_character);
+/*
 var fighter_character = new Character("Fighter", "green.png");
 player_party.add_member(fighter_character);
-
+*/
 var enemy_character1 = new Character("Enemy", "red.png");
 enemy_party.add_member(enemy_character1);
+/*
 var enemy_character2 = new Character("Enemy", "red.png");
 enemy_party.add_member(enemy_character2);
 var enemy_character3 = new Character("Enemy", "red.png");
 enemy_party.add_member(enemy_character3);
-
-
+*/
+var Combat_State = {
+    Characer_Select: 0,
+    Action_Select: 1,
+    Target_Select: 2,
+    Player_Animaion: 3,
+    Enemy_Calculate_Turn: 4,
+    Enemy_Animation: 5
+};
 var combat = {
+    state: 0,
     scene: new Scene("Combat", 1.0, function(){
 	// set position of all characters in player_party and enemy_party
 	var renderables = [];
 	var character_selection_options = [];
-	
 	for(var i = 0; i < player_party.characters.length; ++i){
 	    // Set positions from center-screen, right to left
 	    player_party.characters[i].position = 
@@ -35,18 +44,44 @@ var combat = {
 			   enemy_party.characters[i].size.x, 0.2);
 	    renderables.push(enemy_party.characters[i]);
 	}
-	
 	// Add ui renderables
 	renderables.push(combat.action_selection_col);
 	renderables.push(combat.first_attack);
 	renderables.push(combat.second_attack);
-
 	combat.character_selection = new Selection(character_selection_options);
-		
 	combat.scene.set_renderables(renderables);
-	
+	combat.state = Combat_State.Character_Select;
     }, function (delta_s) {
-	// Update enemy logic if needed and play animations
+	switch(combat.state){
+	case Combat_State.Character_Select:
+	    // Select Character to use action
+	    // Character selection should be visible
+	    break;
+	case Combat_State.Action_Select:
+	    // Select action to use
+	    // Action selection should be visible
+	    break;
+	case Combat_State.Target_Select:
+	    // Select target to use action on
+	    // Either Character selection or Enemy selection should be visible
+	    //   Character for healing, of Enemy for damage
+	    break;
+	case Combat_State.Player_Animation:
+	    // Update and check timeline
+	    break;
+	case Combat_State.Enemy_Calculate_Turn:
+	    // Calculate Enemy turn
+	    
+	    // Calculate enemy to use action
+	    // Calculate enemy action
+	    // Calculate enemy's target
+	    break;
+	case Combat_State.Enemy_Animation:
+	    // Update and check timeline
+	    break;
+	default:
+	    break;
+	}
 	
 	// Set first_attack and second_attack to pngs based on character_selection
     }),
