639bd0f07d287343b9ea547195da72fd89590c37
Kim <Kpampusch@yahoo.com>
2017-12-16 23:10:02 -0600

    changed dungeon_key to be picked up by bb intersection insted of active interaction

--- js/dungeon_map.js
+++ js/dungeon_map.js
@@ -27,19 +27,23 @@ dungeon.add_actor(new Actor(new Vector(dungeon_width - 0.5, 0.0),
 			    new Animation("black", Sprite.black)));
 
 
-var dungeon_key_item = new Inventory_Item("dungeon key", "key from a skeleton");
-// The skeleton that the player recieves the key from
-function dungeon_key_interaction () {
-    if(!Inventory.contains(dungeon_key_item)){
-	Alert.set("Picked up a key!");
-	Inventory.add_item(dungeon_key_item);
-	dungeon_key.hide();
-    }
-}
+var dungeon_key_item = new Inventory_Item("dungeon key", "key on the ground");
 var dungeon_key = new Actor(new Vector(3.5, 1.5), new Vector(0.5, 0.5),
 			    new Animation("key.png", Sprite.key),
-			    false, false, function(){}, dungeon_key_interaction);
+			    false);
 dungeon.add_actor(dungeon_key);
+function dungeon_key_pickup_test () {
+    return dungeon_key.bounding_box.detect_intersection(slime.bounding_box) ==
+	block_relative_position.intersects && !Inventory.contains(dungeon_key_item);
+}
+function dungeon_key_pickup_callback () {
+    Alert.set("Picked up a key!");
+    Inventory.add_item(dungeon_key_item);
+    dungeon_key.hide();
+}
+var dungeon_key_pickup_event = new Event(dungeon_key_pickup_test,
+					 dungeon_key_pickup_callback);
+dungeon.add_event(dungeon_key_pickup_event);
 
 var dungeon_gate = new Actor(new Vector(2.0, 0.0), new Vector(0.75, 1.0),
 			     new Animation("gate.png", Sprite.gate));
@@ -69,6 +73,7 @@ function dungeon_gate_unlock_callback () {
 var dungeon_door_unlock_event =
     new Event(dungeon_gate_unlock_test, dungeon_gate_unlock_callback);
 dungeon.add_event(dungeon_door_unlock_event);
+
 var dungeon_exit_hitbox = new Block(new Vector(2.0, 0.0),
 				    new Vector(0.75, 0.01));
 function dungeon_exit_test () {
